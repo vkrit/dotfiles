3a22d5fd05bda6ead812621444db01a335f1b76f
Veerasak Kritsanapraphan <vkrit@yahoo.com>
2022-09-15 16:26:59 +0700

    Fix user on nitial commit

---
+++ dot_config/lvim/config.lua
@@ -0,0 +1,193 @@
+-- general
+vim.opt.relativenumber = true
+lvim.log.level = "warn"
+lvim.format_on_save = true
+lvim.leader = "space"
+lvim.keys.insert_mode["ii"] = "<Esc>"
+lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+lvim.builtin.orgmode = { active = false }
+
+-- Dashboard Configuration File
+require('user.dashboard')
+
+-- Visuals Configuration File
+require('user.visuals')
+
+-- Terminal Configuration File
+require('user.terminal')
+
+-- LuaLine Configuration File
+require('user.lualine')
+
+-- intellisense Configuration File
+require('user.intellisense')
+
+--Orgmode
+require('user.orgmode')
+
+local _, actions = pcall(require, "telescope.actions")
+lvim.builtin.telescope.defaults.mappings = {
+  -- for input mode
+  i = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+    ["<C-n>"] = actions.cycle_history_next,
+    ["<C-p>"] = actions.cycle_history_prev,
+  },
+  -- for normal mode
+  n = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+  },
+}
+
+lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+lvim.builtin.which_key.mappings["t"] = {
+  name = "+Trouble",
+  r = { "<cmd>Trouble lsp_references<cr>", "References" },
+  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+  d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+  w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
+}
+
+-- -- change UI setting of `LspInstallInfo`
+-- -- see <https://github.com/williamboman/nvim-lsp-installer#default-configuration>
+-- lvim.lsp.installer.setup.ui.check_outdated_servers_on_open = false
+-- lvim.lsp.installer.setup.ui.border = "rounded"
+-- lvim.lsp.installer.setup.ui.keymaps = {
+--     uninstall_server = "d",
+--     toggle_server_expand = "o",
+-- }
+
+-- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
+-- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
+-- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
+-- local opts = {} -- check the lspconfig documentation for a list of all possible options
+-- require("lvim.lsp.manager").setup("pyright", opts)
+
+-- -- you can set a custom on_attach function that will be used for all the language servers
+-- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
+-- lvim.lsp.on_attach_callback = function(client, bufnr)
+--   local function buf_set_option(...)
+--     vim.api.nvim_buf_set_option(bufnr, ...)
+--   end
+--   --Enable completion triggered by <c-x><c-o>
+--   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+-- end
+
+-- Additional Plugins
+lvim.plugins = {
+  { -- Theme
+    "folke/tokyonight.nvim"
+  },
+  { -- Plugins for databases
+    "kristijanhusak/vim-dadbod-ui",
+    requires = {
+      "tpope/vim-dadbod",
+      "kristijanhusak/vim-dadbod-completion"
+    }
+  },
+  {
+    "nvim-orgmode/orgmode",
+    keys = { "go", "gC" },
+    ft = { "org" },
+    config = function()
+      require("user.orgmode").setup()
+    end
+  },
+  -- { -- Scala support
+  --   "scalameta/nvim-metals",
+  --   config = function()
+  --     require("user.metals").config()
+  --   end,
+  -- },
+
+}
+
+-- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
+-- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
+-- local _, actions = pcall(require, "telescope.actions")
+-- lvim.builtin.telescope.defaults.mappings = {
+--   -- for input mode
+--   i = {
+--     ["<C-j>"] = actions.move_selection_next,
+--     ["<C-k>"] = actions.move_selection_previous,
+--     ["<C-n>"] = actions.cycle_history_next,
+--     ["<C-p>"] = actions.cycle_history_prev,
+--   },
+--   -- for normal mode
+--   n = {
+--     ["<C-j>"] = actions.move_selection_next,
+--     ["<C-k>"] = actions.move_selection_previous,
+--   },
+-- }
+
+-- Use which-key to add extra bindings with the leader-key prefix
+lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+lvim.builtin.which_key.mappings["t"] = {
+  name = "+Trouble",
+  r = { "<cmd>Trouble lsp_references<cr>", "References" },
+  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+  d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+  w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
+}
+lvim.builtin.which_key.mappings["d"] = {
+  name = "Debug",
+  s = {
+    name = "Step",
+    c = { "<cmd>lua require('dap').continue()<CR>", "Continue" },
+    v = { "<cmd>lua require('dap').step_over()<CR>", "Step Over" },
+    i = { "<cmd>lua require('dap').step_into()<CR>", "Step Into" },
+    o = { "<cmd>lua require('dap').step_out()<CR>", "Step Out" },
+  },
+  h = {
+    name = "Hover",
+    h = { "<cmd>lua require('dap.ui.variables').hover()<CR>", "Hover" },
+    v = { "<cmd>lua require('dap.ui.variables').visual_hover()<CR>", "Visual Hover" },
+  },
+  u = {
+    name = "UI",
+    h = { "<cmd>lua require('dap.ui.widgets').hover()<CR>", "Hover" },
+    f = { "local widgets=require('dap.ui.widgets');widgets.centered_float(widgets.scopes)<CR>", "Float" },
+  },
+  r = {
+    name = "Repl",
+    o = { "<cmd>lua require('dap').repl.open()<CR>", "Open" },
+    l = { "<cmd>lua require('dap').repl.run_last()<CR>", "Run Last" },
+  },
+  b = {
+    name = "Breakpoints",
+    c = {
+      "<cmd>lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+      "Breakpoint Condition",
+    },
+    m = {
+      "<cmd>lua require('dap').set_breakpoint({ nil, nil, vim.fn.input('Log point message: ') })<CR>",
+      "Log Point Message",
+    },
+    t = { "<cmd>lua require('dap').toggle_breakpoint()<CR>", "Create" },
+  },
+  c = { "<cmd>lua require('dap').scopes()<CR>", "Scopes" },
+  i = { "<cmd>lua require('dap').toggle()<CR>", "Toggle" },
+}
+
+-- Autocommands (https://neovim.io/doc/user/autocmd.html)
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = { "*.json", "*.jsonc" },
+  -- enable wrap mode for json files only
+  command = "setlocal wrap",
+})
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "zsh",
+  callback = function()
+    -- let treesitter use bash highlight for zsh files as well
+    require("nvim-treesitter.highlight").attach(0, "bash")
+  end,
+})
+
+lvim.builtin.dap.active = true
