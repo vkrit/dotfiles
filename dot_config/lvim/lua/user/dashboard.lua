b7bef8e21561406cf50941ee842ca5509ec65275
Kris Kritsanapraphan <veerasak.kritsanapraphan@gmail.com>
2022-09-15 19:53:09 +0700

    Change dashboard

--- dot_config/lvim/lua/user/dashboard.lua
+++ dot_config/lvim/lua/user/dashboard.lua
@@ -40,14 +40,11 @@ lvim.builtin.alpha.dashboard.section.buttons.entries[1] = lvim.builtin.alpha.das
 lvim.builtin.alpha.dashboard.section.buttons.entries[2] = lvim.builtin.alpha.dashboard.section.buttons.entries[4]
 lvim.builtin.alpha.dashboard.section.buttons.entries[4] = { "SPC d", "  Database Mode", "<CMD>DBUI<CR>" }
 lvim.builtin.alpha.dashboard.section.buttons.entries[5] = { "SPC ", "  Org Mode", "<CMD>lua print('lol')<cr>" }
-lvim.builtin.alpha.dashboard.section.buttons.entries[1] = lvim.builtin.alpha.dashboard.section.buttons.entries[2]
-lvim.builtin.alpha.dashboard.section.buttons.entries[2] = lvim.builtin.alpha.dashboard.section.buttons.entries[4]
-lvim.builtin.alpha.dashboard.section.buttons.entries[4] = { "SPC d", "  Database Mode", "<CMD>DBUI<CR>" }
-lvim.builtin.alpha.dashboard.section.buttons.entries[5] = { "SPC ", "  Org Mode", "<CMD>lua print('lol')<cr>" }
 
 local text = require "lvim.interface.text"
 lvim.builtin.alpha.dashboard.section.footer.val = text.align_center({ width = 0 }, {
-  "Talk is cheap. Show me the code! - Linus Torvalds",
+  "Stay Hungry Stay Foolish! - Steve Jobs",
+  "24/2/1955-10/2/2011",
   "",
   "               Powered by Lunarvim               ",
   require("lvim.utils.git").get_lvim_version()
